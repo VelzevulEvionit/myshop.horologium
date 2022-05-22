d3c3bdb8d6514be78e1d90852df67191818d8542
Laleila Absolutte <82674989+VelzevulEvionit@users.noreply.github.com>
2022-05-22 18:27:41 +0300

    Add files via upload

---
+++ config/params.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'admin_email' => 'haced17.0@gmail.com',
+    'shop_name' => 'myshop horologium',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => '465',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'myshop.horologium@gmail.com',
+    'smtp_password' => 'ddxuwgvjyemuvqba',
+    'img_width' => 125,
+    'img_height' => 200,
+    'gallery_width' => 700,
+    'gallery_height' => 1000,
+];
